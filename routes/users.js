3b3269705c9e86ccfb1b5eab938a0a63b58fbd93
Filipe Abrantes <filipenabrantes@gmail.com>
2019-11-25 11:38:41 -0300

    refatoração de funções com async await

--- routes/users.js
+++ routes/users.js
@@ -10,41 +10,42 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const { email, password } = req.body;
 
     if(!email || !password) return res.send({ error: 'Dados insuficientes' });
 
-    Users.findOne({ email }, (err, data) => {
-        if(err) return res.send({ error: 'Erro ao buscar usuário' });
-        if (data) return res.send({ error: 'Usuário já registrado' });
+    try {
+        if (await Users.findOne({ email })) return res.send({ error: 'Usuario já registrado' });
 
-        Users.create(req.body, (err, data) => {
-            if (err) return res.send({ error: 'Erro ao criar usuário' });
+        const user = await Users.create(req.body);
+        user.password = undefined;
+        return res.send(user);
+    }
+    catch (err) {
+        if (err) return res.send({ error: 'Erro ao buscar usuário' });
+    }
+})
 
-            data.password = undefined;
-            return res.send(data);
-        })
-    })
-});
-
-router.post('/auth', (req, res) => {
+router.post('/auth', async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) return res.send({ error: 'Dados insuficientes' });
 
-    Users.findOne({ email }, (err, data) => {
-        if (err) return res.send({ error: 'Erro ao buscar usuário' });
-        if (!data) return res.send({ error: 'Usuário não registrado' });
+    try {
+        const user = await Users.findOne({ email }).select('+password');
+        if (!user) return res.send({ error: 'Usuário não registrado' });
 
-        bcrypt.compare(password, data.password, (err, same) => {
-            if (!same) return res.send({ error: 'Erro ao autenticar usuário' });
+        const pass_ok = await bcrypt.compare(password, user.password);
 
-            data.password = undefined;
+        if (!pass_ok) return res.send({ error: 'Erro ao autenticar usuário' });
 
-            return res.send(data);
-        })
-    }).select('+password');
+        user.password = undefined;
+        return res.send(user);
+
+    } catch (err) {
+        return res.send({ error: 'Erro ao buscar usuário' });
+    }
 });
 
 module.exports = router; 
